ca5713cac4c15a74743a4fbf256628cb67aa6179
AntonTerehovPerm <41608732+AntonTerehovPerm@users.noreply.github.com>
2019-11-10 14:08:44 +0300

    new_vector

---
+++ variant 2/elf.js
@@ -0,0 +1,20 @@
+﻿var cnv = document.getElementById('canvas'),
+	ctx = canv.getContext('2d');
+	
+var	trava = new Image();
+	trava.src = "name.png";
+	
+cnv.width = window.innerWidth;
+cnv.height = window.innerHeight;
+	
+var	x = 0;
+var	y = 0;
+var	h = window.innerHeight;
+var	w = window.innerWidth;
+	
+	setInterval(function(){
+		ctx.moveTo(x,y);
+		ctx.lineTo(x,h);
+		x += 20
+	},
+	10); 
